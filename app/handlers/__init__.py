dbb65cfe4715c74d69c8302e233461ff15cc442b
Rowan Walton <121184757+thebeigerabbit@users.noreply.github.com>
2026-03-21 17:52:57 +0200

    Create __init__.py

---
+++ app/handlers/__init__.py
@@ -0,0 +1,7 @@
+from .auth import start_handler, signup_handler, login_handler
+from .partner import add_partner_handler, accept_partner_handler, reject_partner_handler
+from .checkin import yes_handler, no_handler
+from .reflection import reflect_handler
+from .urge import urge_handler, urge_followup_callback
+from .report import report_handler, help_handler
+from .dispatcher import message_dispatcher
